e4168e2431432094623273dc042167e5da413a52
Aram <me@aram.sh>
2024-05-16 18:48:24 -0700

    Fix failing issues

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ const app = new App({
   socketMode: true,
 });
 
-app.command("/pick", async ({ command, ack, client, body }: any) => {
+app.command("/dev-pick", async ({ command, ack, client, body }: any) => {
   await ack();
 
   const metadata_to_send = { channel: body.channel_id, user: body.user_id };
@@ -73,9 +73,6 @@ app.view("pick", async ({ ack, body, client }: any) => {
 
         if (result.user.is_bot) {
           toremove.push(member);
-          // why append to another array and THEN delete? Because
-          // if you delete in a for in loop where the in is what
-          // for is what your removing you will skip over results
         }
       } catch (error) {
         console.error(error);
@@ -83,7 +80,6 @@ app.view("pick", async ({ ack, body, client }: any) => {
     }
 
     for (const remove in toremove) {
-      // bot removal code
       members.pop(remove);
     }
 
