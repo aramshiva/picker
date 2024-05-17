56a7e45d9d740d2e0326f0a178dc9f4613622f46
Aram <me@aram.sh>
2024-05-16 17:26:52 -0700

    Add User Logging

--- app.ts
+++ app.ts
@@ -11,6 +11,10 @@ const app = new App({
 app.command("/pick", async ({ command, ack, client, body }: any) => {
   await ack();
 
+  console.log(body.user_id + " picked a random person!");
+  const metadata_to_send = { channel: body.channel_id, user: body.user_id };
+  const metadata = JSON.stringify(metadata_to_send);
+
   try {
     const result = await client.views.open({
       trigger_id: body.trigger_id,
@@ -21,7 +25,7 @@ app.command("/pick", async ({ command, ack, client, body }: any) => {
           type: "plain_text",
           text: "Picker",
         },
-        private_metadata: body.channel_id,
+        private_metadata: metadata,
         blocks: [
           {
             type: "input",
@@ -50,7 +54,9 @@ app.command("/pick", async ({ command, ack, client, body }: any) => {
 
 app.view("pick", async ({ ack, body, client }: any) => {
   await ack();
-  const channel = body.view.private_metadata;
+  const metadata = JSON.parse(body.view.private_metadata);
+  const channel = metadata["channel"];
+  const user = metadata["user"];
   const values = body.view.state.values;
   const reason = values.input.reason_input.value;
 
@@ -62,7 +68,7 @@ app.view("pick", async ({ ack, body, client }: any) => {
 
     await client.chat.postMessage({
       channel: channel,
-      text: `Hey <@${picked}>! You have been picked for ${reason}`,
+      text: `Hey <@${picked}>! You have been picked for ${reason} by <@${user}>`,
     });
   } catch (error) {
     console.error("Failed to pick a member:", error);
